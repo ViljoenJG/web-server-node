4a79057ec788823742a42a138ed853a3259ba602
Kobus Viljoen <kobusv@gmail.com>
2017-06-12 20:49:41 +0200

    added logging of all incoming requests

--- server.js
+++ server.js
@@ -4,12 +4,19 @@ const hbs = require('hbs');
 let app = express();
 const port = 3000;
 
-hbs.registerPartials(__dirname + '/views/partials')
-app.set('view engine', 'hbs')
-app.use(express.static(__dirname + '/public'))
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  const now = new Date().toString();
+
+  console.log(`${ now } - ${ req.method } ${ req.url }`);
+  next();
+});
 
 hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
-hbs.registerHelper('screamIt', txt => txt.toUpperCase())
+hbs.registerHelper('screamIt', txt => txt.toUpperCase());
 
 app.get('/', (req, res, next) => {
   res.render('home.hbs', {
